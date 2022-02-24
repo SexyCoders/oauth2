5a541b3ed362c26bdc6e0fb73ed14f5e5e6e9533
Shane <sotiris@sexycoders.org>
2022-02-24 12:47:02 +0200

    changed to post and added cors

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require __DIR__ . '/vendor/autoload.php';
+require './vendor/autoload.php';
 
 $username='libauth';
 $filename='/etc/libauth.js/oauth_pass';
