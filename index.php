a8f21d69455c5e239ca7a097149602abc713b1f6
Shane <sotiris@sexycoders.org>
2022-03-16 15:50:14 +0200

    working on redis user server

--- index.php
+++ index.php
@@ -59,10 +59,14 @@ $app->post('/token',function(Request $request, Response $response){
     // @ generate a Oauth 2.0 token in json with format below
     // @ {"access_token":"ac7aeb0ee432bf9b73f78985c66a1ad878593530","expires_in":3600,"token_type":"Bearer","scope":null}
     $t=OAuth2\Request::createFromGlobals();
-    $pp=fopen("/tmp/tttttt","w");
-    fwrite($pp,$t);
+    $g=json_decode($t);
+    $j=$request->getBody();
+    $a=[];
+    parse_str($j,$a) 
+    $redis = new Redis();
+    $redis->connect('10.0.0.250', 6379);
+    $redis->set($g->access_token,$a['client_id']);
     $server->handleTokenRequest($t)->send();
-    
 
 });
 
